e983f3d27671d90a33db999382a22e20660c3ab2
Redyf <mateusalvespereira7@gmail.com>
2024-01-12 13:58:34 -0300

    feat: Add networking options

--- systems/x86_64-linux/redyf/default.nix
+++ systems/x86_64-linux/redyf/default.nix
@@ -86,6 +86,20 @@
     };
   };
 
+  networking = {
+    networkmanager.enable = true;
+    enableIPv6 = false;
+    # no need to wait interfaces to have an IP to continue booting
+    dhcpcd.wait = "background";
+    # avoid checking if IP is already taken to boot a few seconds faster
+    dhcpcd.extraConfig = "noarp";
+    hostName = "nixos"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
   # Enables docker in rootless mode
   virtualisation = {
     docker.rootless = {
