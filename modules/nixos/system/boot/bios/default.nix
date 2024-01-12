e5bf52467acc9263b665aedf2406a2f77d52e013
Redyf <mateusalvespereira7@gmail.com>
2024-01-12 14:55:53 -0300

    Remove deploy-rs to test a theory

--- modules/nixos/system/boot/bios/default.nix
+++ modules/nixos/system/boot/bios/default.nix
@@ -10,7 +10,7 @@ with lib.custom; let
 in {
   options.system.boot.bios = with types; {
     enable = mkBoolOpt false "Whether or not to enable bios booting.";
-    device = mkOpt str "/dev/sda" "Disk that grub will be installed to.";
+    device = mkOpt str "nodev" "Disk that grub will be installed to.";
   };
 
   config = mkIf cfg.enable {
