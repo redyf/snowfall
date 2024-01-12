ac4a4cb0e3c2a6559f1843320d496f48dd4ce60a
Redyf <mateusalvespereira7@gmail.com>
2024-01-12 14:10:55 -0300

    feat: Add extraOptions for git (I can't take this anymore)

--- modules/nixos/apps/git/default.nix
+++ modules/nixos/apps/git/default.nix
@@ -14,7 +14,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.git = {
+    home.extraOptions.git = {
       enable = true;
       userName = "Redyf";
       userEmail = "mateusalvespereira7@gmail.com";
